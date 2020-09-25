a596a9c5925f7008efc7d3013a138adf79c2b63b
lucas delfini <delfini.lucas@hotmail.com>
2020-09-25 00:42:34 -0300

    Usuarios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,9 @@
       <link rel="stylesheet" href="/css/materialize.min.css">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
       <nav>
     <div class="nav-wrapper">
       <a href="#" class="brand-logo">Logo</a>
@@ -21,10 +20,132 @@
     </div>
   </nav>
     </head>
+  <center>
+    <div class="container">
+    <div class="row ">
+    <div class="col s12 m2" style="width: 20%">
+      <div class="card">
+        <div class="card-image">
+          <img src="img/carina.jpg">
+        </div>
+        <div class="card-content">
+        <h5> carina </h5>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action" >
+        <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
+        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
+        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m2" style="width: 20%">
+    <div class="card">
+        <div class="card-image">
+          <img src="img/lucas.jpg">
+          
+        </div>
+        <div class="card-content">
+        <h5> Lucas </h5>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        
+        <div class="card-action">
+                <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
+        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
+        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
+        </div>
+      
+      </div>
+    </div>
+    <div class="col s12 m2" style="width: 20%">
+    <div class="card">
+        <div class="card-image">
+          <img src="img/juan.jpg">
+         
+        </div>
+        <div class="card-content">
+        <h5> Juan </h5>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+                <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
+        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
+        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m2" style="width: 20%">
+    <div class="card">
+        <div class="card-image">
+          <img src="img/herica.jpg">
+         
+        </div>
+        <div class="card-content">
+        <h5> Herica <sub><i>(com h)</i></sub> </h5>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+                <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
+        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
+        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m2" style="width: 20%">
+    <div class="card">
+        <div class="card-image">
+          <img src="img/gui.jpg">
+          
+        </div>
+        <div class="card-content">
+        <h5> Guilherme </h5>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+                <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
+        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
+        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
+        </div>
+      </div>
+    </div>
+    </div>
+    
+  </div>
+  </center>
+   
 
-    <body>
+ 
+  <div class="fixed-action-btn">
+  <a class="btn-floating btn-large red">
+    <i class="large material-icons">mode_edit</i>
+  </a>
+  <ul>
+    <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+    <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+    <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+    <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+  </ul>
+</div>
+
+            
 
       <!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script src="js/jquery.min.js"></script>
+    <script src="js/materialize.min.js"></script>    
+    <script type="text/javascript">
+     $(document).ready(function(){
+     $('.fixed-action-btn').floatingActionButton();
+     $(".dropdown-trigger").dropdown();
+    });
+
+
+        
+    </script>
     </body>
   </html>
