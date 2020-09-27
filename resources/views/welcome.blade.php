ecc068c26a0374dfcd7f5e917afcfd2219e46bf7
lucas delfini <delfini.lucas@hotmail.com>
2020-09-27 02:29:43 -0300

    commit 27-09

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,143 +9,352 @@
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <nav>
-    <div class="nav-wrapper">
+      
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+      <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script>
+jQuery(document).ready(function($) { 
+    $(".scroll").click(function(event){        
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600);
+   });
+});
+</script>
+ 
+
+  
+    </head>
+  <body>
+  <div class="navbar-fixed">
+    <nav>
+    <div class="nav-background">
+        <div class="ea k" style="background-image: url('//cdn.shopify.com/s/files/1/1775/8583/t/1/assets/icon-seamless.png?v=16169594983224640439');"></div>
+      </div>
+    <div class="nav-wrapper yellow darken-2">
       <a href="#" class="brand-logo">Logo</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
+        <li><a class="scroll" href="/#inicio">inicio</a></li>
+        <li><a class="scroll" href="/#app1">APP 1</a></li>
+        <li><a class="scroll" href="/#app2">APP 2</a></li>
+        <li><a class="scroll" href="/#app3">APP 3</a></li>
+        <li><a class="scroll"href="/#equipe">equipe</a></li>
+        <li><a class="scroll"href="collapsible.html">JavaScript</a></li>
       </ul>
     </div>
   </nav>
-    </head>
-  <center>
-    <div class="container">
-    <div class="row ">
-    <div class="col s12 m2" style="width: 20%">
-      <div class="card">
-        <div class="card-image">
-          <img src="img/carina.jpg">
-        </div>
-        <div class="card-content">
-        <h5> carina </h5>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action" >
-        <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
-        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
-        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
-        </div>
-      </div>
-    </div>
-    <div class="col s12 m2" style="width: 20%">
-    <div class="card">
-        <div class="card-image">
-          <img src="img/lucas.jpg">
-          
-        </div>
-        <div class="card-content">
-        <h5> Lucas </h5>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        
-        <div class="card-action">
-                <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
-        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
-        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
-        </div>
-      
-      </div>
-    </div>
-    <div class="col s12 m2" style="width: 20%">
-    <div class="card">
-        <div class="card-image">
-          <img src="img/juan.jpg">
-         
-        </div>
-        <div class="card-content">
-        <h5> Juan </h5>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-                <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
-        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
-        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
-        </div>
-      </div>
-    </div>
-    <div class="col s12 m2" style="width: 20%">
-    <div class="card">
-        <div class="card-image">
-          <img src="img/herica.jpg">
-         
-        </div>
-        <div class="card-content">
-        <h5> Herica <sub><i>(com h)</i></sub> </h5>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-                <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
-        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
-        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
-        </div>
-      </div>
-    </div>
-    <div class="col s12 m2" style="width: 20%">
-    <div class="card">
-        <div class="card-image">
-          <img src="img/gui.jpg">
-          
-        </div>
-        <div class="card-content">
-        <h5> Guilherme </h5>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-                <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
-        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
-        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
-        </div>
-      </div>
-    </div>
-    </div>
-    
   </div>
-  </center>
-   
+
+  <div class="slider">
+    <ul class="slides">
+      <li>
+        <img src="img/carrosel 1.jpeg"> <!-- random image -->
+        <div class="caption right-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="img/carrosel 2.jpeg"> <!-- random image -->
+        <div class="caption left-align">
+          <h3>Left Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://lorempixel.com/580/250/nature/3"> <!-- random image -->
+        <div class="caption right-align">
+          <h3>Right Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://lorempixel.com/580/250/nature/4"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+  <br  id="inicio">
+    <div class="container">
+    <div class="section white" >
+      <div class="row container" >
+      <h1 class="header center orange-text">Nome do Projeto Aqui</h1>
+      <div class="row center">
+        <h5 class="header col s12 light">Breve descrição a respeito do projeto </h5>
+      </div>
+      <div class="row center">
+      <p> Veja um video sobre nosso projeto </p>
+      <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+      </div>
+      </div>
+    </div>
+    </div>
+  <div class="parallax-container">
+      <div class="parallax"><img src="img/floresta2.jpg"></div>
+    </div>
+ 
+        
+
 
  
-  <div class="fixed-action-btn">
-  <a class="btn-floating btn-large red">
-    <i class="large material-icons">mode_edit</i>
-  </a>
-  <ul>
-    <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
-    <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
-    <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
-    <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
-  </ul>
-</div>
+<div class="row center">
+          <!-- Modal Trigger -->
+<!-- Modal Structure -->
+<div id="modal1" class="modal">
+  <div class="modal-content">
+    <h4>Modal Header  <a href="#!" class="modal-close material-icons right">close</a></h4> 
+    <div class="video-container">
+    <iframe width="853" height="480" src="https://www.youtube.com/embed/G1IbRujko-A" frameborder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+      <br><br>
 
-            
+    </div>
+  </div>
+<br>
+<br  id="app1">
+<div class="container" >
+    <div class="section white" >
+      <div class="row container" >
+      <h1 class="header center orange-text">APP 1</h1>
+      <div class="row center">
+        <h5 class="header col s12 light">Breve descrição a respeito do projeto </h5>
+      </div>
+      <div class="row center">
+      <p> Veja um video sobre nosso projeto </p>
+      <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+      </div>
+      </div>
+    </div>
+    </div>
 
-      <!--JavaScript at end of body for optimized loading-->
-      <script src="js/jquery.min.js"></script>
+
+<br>
+  <div class="parallax-container">
+      <div class="parallax"><img src="img/floresta.jpg"></div>
+    </div>
+<br>
+<br id="app2">
+    <div class="container" id="app2">
+    <div class="section white" >
+      <div class="row container" >
+      <h1 class="header center orange-text">APP 2</h1>
+      <div class="row center">
+        <h5 class="header col s12 light">Breve descrição a respeito do projeto </h5>
+      </div>
+      <div class="row center">
+      <p> Veja um video sobre nosso projeto </p>
+      <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+      </div>
+      </div>
+    </div>
+    </div>
+<br>
+<div class="parallax-container">
+      <div class="parallax"><img src="img/floresta.jpg"></div>
+    </div>
+    <br id="app3">
+    <div class="container">
+    <div class="section white" >
+      <div class="row container" >
+      <h1 class="header center orange-text">APP 3</h1>
+      <div class="row center">
+        <h5 class="header col s12 light">Breve descrição a respeito do projeto </h5>
+      </div>
+      <div class="row center">
+      <p> Veja um video sobre nosso projeto </p>
+      <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+      </div>
+      </div>
+    </div>
+    </div>
+    <br>
+    <div class="parallax-container">
+      <div class="parallax"><img src="img/floresta.jpg"></div>
+    </div>
+    <br>
+
+  
+  <center>
+
+  <h3 id="equipe" > A equipe </h3>
+  <div class="container" >
+<div class="row">
+  <div class="col s12 m2" style="width: 20%">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/carina.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Carina Rodrigues</span>
+      <br>
+      <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
+      &nbsp; 
+        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
+        
+        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
+        
+        <a href="mailto:email_que_recebe_as_mensagens@exemplo.com?Subject=Título%20da%20mensagem">  <img src="img/email3.png" height="35" width="35"> </img> </a>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Quem sou?<i class="material-icons right">close</i></span>
+      <p> 20 anos, estudante em bacharel em sistemas de informação, estou no inicio da minha carreira na área de Tecnologia e sempre em busca de novos conhecimentos para me aperfeiçoar como profissional. Sucesso é o acúmulo de pequenos esforços, repetidos dia a dia.</p>
+    </div>
+  </div>
+  </div>
+
+  <div class="col s12 m2" style="width: 20%">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/gui.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Guilherme Reis</span>
+      <br>
+      <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
+      &nbsp; 
+        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
+        
+        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
+        
+        <a href="mailto:email_que_recebe_as_mensagens@exemplo.com?Subject=Título%20da%20mensagem">  <img src="img/email3.png" height="35" width="35"> </img> </a>
+
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Quem sou ?<i class="material-icons right">close</i></span>
+      <p> Um cara curioso que procura saber como tudo funciona, apaixonado pela informática, fez curso técnico e atualmente cursa Bacharelado em sistemas de informação na FHO Uniararas.</p>
+    </div>
+  </div>
+  </div>
+
+  <div class="col s12 m2" style="width: 20%">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/herica.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Herica Cadoni</span>
+      <br>
+      <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
+      &nbsp; 
+        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
+        
+        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
+        
+        <a href="mailto:email_que_recebe_as_mensagens@exemplo.com?Subject=Título%20da%20mensagem">  <img src="img/email3.png" height="35" width="35"> </img> </a>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Quem sou ?<i class="material-icons right">close</i></span>
+      <p> Estudante de t.i, apaixonada por animais e software, vivendo em Mogi Guaçu, em busca de novas experiências.</p>
+    </div>
+  </div>
+  </div>
+
+  <div class="col s12 m2 " style="width: 20%">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/juan.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Juan Lopes</span>
+      <br>
+      <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
+      &nbsp; 
+        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
+        
+        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
+        
+        <a href="mailto:email_que_recebe_as_mensagens@exemplo.com?Subject=Título%20da%20mensagem">  <img src="img/email3.png" height="35" width="35"> </img> </a>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Quem sou ?<i class="material-icons right">close</i></span>
+      <p> Futuro Bacharel em sistemas de informação, apaixonado por tecnologia, finanças, aviação e inovação. Amo desafios e trabalho em equipe, está com problemas? Vamos resolver juntos!</p>
+    </div>
+  </div>
+  </div>
+
+  <div class="col s12 m2" style="width: 20%">
+  <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="img/lucas.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Lucas Delfini</span>
+      <br>
+      <img src="img/face.png" height="35" width="35"><a href="#"></a></img>
+      &nbsp; 
+        <img src="img/likedin.png" height="35" width="35"><a href="#"></a></img>
+        
+        <img src="img/git.png" height="35" width="35"><a href="#"></a></img>
+        
+        <a href="mailto:email_que_recebe_as_mensagens@exemplo.com?Subject=Título%20da%20mensagem">  <img src="img/email3.png" height="35" width="35"> </img> </a>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Quem sou ?<i class="material-icons right">close</i></span>
+      <p>20 anos cursando sistemas da informação. Apaixonado por informática e cultura pop e como diz uma frase de um famoso jogo:  "Todos nós fazemos escolhas na vida, mas no final nossas escolhas nos fazem" então porquê não nos conhecemos um pouco melhor?</p>
+    </div>
+  </div>
+  </div>
+  </div>
+  </div>
+
+  <script src="js/jquery.min.js"></script>
     <script src="js/materialize.min.js"></script>    
     <script type="text/javascript">
      $(document).ready(function(){
      $('.fixed-action-btn').floatingActionButton();
      $(".dropdown-trigger").dropdown();
+     $('.slider').slider({
+      indicators: false 
+     });
+     $(document).ready(function(){
+    $('.parallax').parallax();
+  });
     });
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options );
+  });
 
+  // Or with jQuery
 
-        
+  $(document).ready(function(){
+    $('.modal').modal();
+  });s    
+
+  
+  
+
     </script>
+
+
+<footer class="page-footer yellow darken-2">
+          <div class="container " >
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
     </body>
   </html>
